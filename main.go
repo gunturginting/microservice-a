69da475674e65d3dbd5764c65fc4018fc8290c8b
gunturginting <anandaguntur18@gmail.com>
2024-04-01 16:29:42 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	// Menggunakan fungsi Static untuk mengakses file statis seperti index.html
+	e.Static("/", "public")
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
